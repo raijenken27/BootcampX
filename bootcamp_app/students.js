00b9bc4109bd930c601ddfd1f401cfaa85a681fc
Jerome Garcia <jeromeo.garcia@gmail.com>
2023-11-22 20:48:31 -0700

    Updating students

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,18 +1,36 @@
-const { Client } = require('pg');
-
-const client = new Client({
-  user: 'vagrant',
-  password: '123',
+const {Pool} = require('pg');
+const pool = new Pool({
+  user: 'labber',
+  password: 'labber',
   host: 'localhost',
   database: 'bootcampx'
 });
 
-pool.query(`
-SELECT id, name, cohort_id
+const queryString = `
+
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
 FROM students
-LIMIT 5;
-`)
-.then(res => {
-  console.log(res);
-})
-.catch(err => console.error('query error', err.stack));
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name LIKE $1
+LIMIT $2;
+
+`;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+const values = [`%${cohortName}%`,limit];
+
+pool.query(queryString,values)
+    .then(res => {
+        // console.log(res.rows);
+        res.rows.forEach(user => {
+          console.log(`
+          
+          ${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort.
+      
+          `);
+        })
+      })
+      .catch(err => {
+        console.log('query error: ',err.stack);
+      });
+
